b6e295a535d71c81cae53d86370050223ae20329
Ethan Walker <ethan.walker830@gmail.com>
2024-07-22 22:47:02 -0700

    add config

---
+++ config.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	xdg "github.com/adrg/xdg"
+	"gopkg.in/yaml.v2"
+)
+
+type Repo struct {
+	Name     string   `yaml:"name,omitempty"`
+	Excludes []string `yaml:"excludes,omitempty"`
+}
+
+type Config struct {
+	GlobalExcludes []string `yaml:"global_excludes,omitempty"`
+	Repos          []Repo   `yaml:"repos,omitempty"`
+}
+
+func defaultExcludes() []string {
+	return []string{".git", "target", "__pyenv__", ".DS_Store"}
+}
+
+func loadConfig() (Config, error) {
+	configFile := filepath.Join(xdg.ConfigHome, "reposync/reposync.yaml")
+
+	_, err := os.Stat(configFile)
+	if os.IsNotExist(err) {
+		cfg := Config{
+			GlobalExcludes: defaultExcludes(),
+		}
+		data, err := yaml.Marshal(cfg)
+		if err != nil {
+			return cfg, err
+		}
+		err = os.Mkdir(filepath.Dir(configFile), 0755)
+		if err != nil {
+			return cfg, err
+		}
+		err = os.WriteFile(configFile, data, 0755)
+		if err != nil {
+			return cfg, err
+		}
+		return cfg, nil
+
+	} else {
+		yamlFile, err := os.ReadFile(configFile)
+		if err != nil {
+			return Config{}, err
+		}
+		cfg := Config{}
+		err = yaml.Unmarshal(yamlFile, &cfg)
+		if err != nil {
+			return Config{}, err
+		}
+		return cfg, nil
+	}
+}
